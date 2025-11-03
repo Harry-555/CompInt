d25945fb8854071bebd7b3346553da9d407af3b5
Harry-555 <harryjackson445@gmail.com>
2025-11-03 22:22:43 +0800

    Solar Intensity

--- main.py
+++ main.py
@@ -10,4 +10,8 @@ energy_demand = ctrl.Antecedent(np.arange(0, 5.1, 0.1), 'energy_demand')
 temperature = ctrl.Antecedent(np.arange(10, 61, 1), 'temperature')
 solar_usage = ctrl.Consequent(np.arange(0, 101, 1), 'solar_usage')
 
-
+# 3 Input
+    # Solar Intensity
+solar_intensity['low'] = fuzz.trapmf(solar_intensity.universe, [0, 0, 150, 400])
+solar_intensity['medium'] = fuzz.trimf(solar_intensity.universe, [300, 500, 700])
+solar_intensity['high'] = fuzz.trapmf(solar_intensity.universe, [600, 850, 1000, 1000])
